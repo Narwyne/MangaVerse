af2be6d811e37a35cad64f67e33b8d797eccdc01
Narwyne <johnnarwynecabrigas@gmaile.com>
2025-10-12 16:03:47 +0800

    styled the search bar and the menu

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Home</title>
-    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+
 </head>
 <body>
     <style>
@@ -32,25 +33,75 @@ body{
     
     
 }
-.item1{
+.header{
     /* header */
     grid-column: 1 / 7;
     height: 50px;
     background-color: rgba(19, 18, 18, 1);
     display: flex;
     justify-content: space-between;
+    /* align-content: center; */
 
 }
-.item1 button{
-    width: 43px;
-    height: 43px;
-    margin-right: 5px;
-    margin-top: 3px;
+.header button {
+  width: 43px;
+  height: 43px;
+  border: none;
+  background: none;
+  color: white;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.item1 input{
-    
+.header button:hover {
+  background-color: rgba(239, 191, 4, 1);
+  border-radius: 5px;
 }
-.item2{
+.material-symbols-outlined {
+  font-size: 28px;
+}
+.search-bar {
+  display: flex;
+  align-items: center;
+  background-color: #4a4a4a; /* dark gray */
+  border-radius: 10px;
+  overflow: hidden;
+  width: 400px; /* adjust width as you like */
+  height: 40px;
+}
+
+.search-bar input {
+  flex: 1;
+  border: none;
+  background: transparent;
+  color: white;
+  padding: 0 10px;
+  outline: none;
+  font-size: 16px;
+}
+
+.search-bar button {
+  background-color: #efbf04; /* yellow */
+  border: none;
+  width: 45px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.search-bar button:hover {
+  background-color: #d4a703; /* darker yellow on hover */
+}
+
+.search-bar .material-symbols-outlined {
+  color: white;
+  font-size: 24px;
+}
+
+.carosel{
     /* recomendation picture 1 */
     grid-column: 1 / 4;
     height: 300px;
@@ -61,7 +112,7 @@ body{
     background-image: url(pictures/Best-Shonen-Mangas-Recommendations.jpg);
     margin-left: 5px;
 }
-.item2p2{
+.caroselp2{
     /* recomendation picture 2 */
     grid-column: 4/7;
     margin-top: 15px;
@@ -105,13 +156,14 @@ body{
     background-color: rgba(64, 64, 64, 1);
     margin-right: 5px;
 }
-.item4top{
+.recommendationsTop{
     /* top10 top */
     grid-column: 6/7;
     height: 40px;
     background-color: rgba(239, 191, 4, 1);
     margin-left: 5px;
     margin-right: 5px;
+    align-content: center;
 }
 
 .item6{
@@ -124,7 +176,7 @@ body{
 }
 .latestMain{
     display: grid;
-    grid-template-columns: auto auto auto;
+    grid-template-columns: 32% 32% 32%;
     grid-template-rows: auto auto auto auto auto;
     column-gap: 10px;
     row-gap: 10px;
@@ -132,23 +184,50 @@ body{
 }
 .content{
     background-color: black;
+    display: flex;
+
+
 }
-    </style>
+.picture{
+    /* background-color: whitesmoke;
+    align-content: flex-start;
+    width: 200px; */
+
+}
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 1,
+  'wght' 700,
+  'GRAD' 200,
+  'opsz' 48
+}
+ </style>
     <div class="container">
-        <div class="item item1">
-            
-            <div id="logo">
-            </div>
-            <input type="text"> 
-            <button></button>
-        </div>
-        <div class="item item2">2</div>
-        <div class="item item2p2">2p2</div>
+                <div class="item header">
+                        <div id="logo"></div>
+
+                            <div class="search-bar">
+                            <input type="text" placeholder="Search...">
+                            <button><span class="material-symbols-outlined">search</span></button>
+                        </div>
+
+                        <button><span class="material-symbols-outlined">menu</span></button>
+                </div>
+        <div class="item carosel">2</div>
+        <div class="item caroselp2">2p2</div>
         <div class="item item3top">  Latest Chapter</div>
-        <div class="item item4top">4top</div>
+        <div class="item recommendationsTop">Recommendations</div>
 
                 <div class="item item3 latestMain">
-                    <div class="content">C1</div>
+                    <div class="content">
+                        <div class="picture"> </div>
+                        <div class="title"></div>
+                        <div class="tags"></div>
+                        <div class="status"></div>
+                    </div>
+
+
+
                     <div class="content">C2</div>
                     <div class="content">C3</div>
                     <div class="content">C4</div>
